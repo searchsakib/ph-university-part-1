2a8361b43201dd7a0a951bb64cfed3d1c5e5324a
searchsakib <searchsakib@gmail.com>
2024-06-02 19:47:59 +0600

    new id count if semester code or year or both changed is done

--- src/app/modules/user/user.utils.ts
+++ src/app/modules/user/user.utils.ts
@@ -16,14 +16,28 @@ const findLastStudentId = async () => {
     })
     .lean();
 
-  return lastStudent?.id ? lastStudent.id.substring(6) : undefined;
+  return lastStudent?.id ? lastStudent.id : undefined;
 };
 
 // find academic semester info
 export const generateStudentId = async (payload: TAcademicSemester) => {
   //first time id 0000
-  const currentId = (await findLastStudentId()) || (0).toString();
+  let currentId = (0).toString();
 
+  const lastStudentId = await findLastStudentId();
+  // 2030 01 0001
+  const lastStudentSemesterCode = lastStudentId?.substring(4, 6); //01
+  const lastStudentYear = lastStudentId?.substring(0, 4); //2030
+  const currentSemestercode = payload.code;
+  const currentYear = payload.year;
+
+  if (
+    lastStudentId &&
+    lastStudentSemesterCode === currentSemestercode &&
+    lastStudentYear === currentYear
+  ) {
+    currentId = lastStudentId.substring(6); // 0001
+  }
   let incrementId = (Number(currentId) + 1).toString().padStart(4, '0');
 
   incrementId = `${payload.year}${payload.code}${incrementId}`;
