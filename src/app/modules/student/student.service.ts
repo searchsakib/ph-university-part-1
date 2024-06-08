22723c0d12804a0f7041ac824e68f00467423797
searchsakib <searchsakib@gmail.com>
2024-06-08 16:50:20 +0600

    dynamically updated non-primitive fields like object

--- src/app/modules/student/student.service.ts
+++ src/app/modules/student/student.service.ts
@@ -33,7 +33,34 @@ const updateSingleStudentIntoDB = async (
   id: string,
   payload: Partial<TStudent>,
 ) => {
-  const result = await Student.findOneAndUpdate({ id }, payload, { new: true });
+  const { name, guardian, localGuardian, ...remainingStudentData } = payload;
+
+  const modifiedUpdatedData: Record<string, unknown> = {
+    ...remainingStudentData,
+  };
+
+  if (name && Object.keys(name).length) {
+    for (const [key, value] of Object.entries(name)) {
+      modifiedUpdatedData[`name.${key}`] = value;
+    }
+  }
+  if (guardian && Object.keys(guardian).length) {
+    for (const [key, value] of Object.entries(guardian)) {
+      modifiedUpdatedData[`guardian.${key}`] = value;
+    }
+  }
+
+  if (localGuardian && Object.keys(localGuardian).length) {
+    for (const [key, value] of Object.entries(localGuardian)) {
+      modifiedUpdatedData[`localGuardian.${key}`] = value;
+    }
+  }
+
+  console.log(modifiedUpdatedData);
+
+  const result = await Student.findOneAndUpdate({ id }, modifiedUpdatedData, {
+    new: true,
+  });
   return result;
 };
 
