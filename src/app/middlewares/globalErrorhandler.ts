5397edbbf91b42db75004243fa194cc800b7aac7
searchsakib <searchsakib@gmail.com>
2024-06-09 12:34:21 +0600

    custom error for zod created

--- src/app/middlewares/globalErrorhandler.ts
+++ src/app/middlewares/globalErrorhandler.ts
@@ -1,23 +1,62 @@
-/* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
 /* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import { NextFunction, Request, Response } from 'express';
+import { ErrorRequestHandler, NextFunction, Request, Response } from 'express';
+import { ZodError, ZodIssue } from 'zod';
+import { TErrorSource } from '../interface/error';
+import { config } from 'dotenv';
 
-const globalErrorHandler = (
-  err: any,
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  const statusCode = err.statusCode || 500;
-  const message = err.message || 'Something went wrong!';
+const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  // setting default values
+  let statusCode = err.statusCode || 500;
+  let message = err.message || 'Something went wrong!';
 
+  let errorSources: TErrorSource = [
+    {
+      path: '',
+      message: 'something went wrong',
+    },
+  ];
+
+  const handleZodError = (err: ZodError) => {
+    const errorSources: TErrorSource = err.issues.map((issue: ZodIssue) => {
+      return {
+        path: issue?.path[issue.path.length - 1],
+        message: issue.message,
+      };
+    });
+    statusCode = 400;
+    return {
+      statusCode,
+      message: 'Validation Error',
+      errorSources,
+    };
+  };
+
+  if (err instanceof ZodError) {
+    const simplifiedError = handleZodError(err);
+    (statusCode = simplifiedError?.statusCode),
+      (message = simplifiedError?.message),
+      (errorSources = simplifiedError?.errorSources);
+  }
+
+  // ultimate error response
   return res.status(statusCode).json({
     success: false,
+    // statusCode,
     message,
-    error: err,
+    errorSources,
+    stack: process.env.NODE_ENV === 'development' ? err?.stack : null,
   });
 };
 
 export default globalErrorHandler;
+
+// patern
+/*
+success
+message
+errorSources:[
+  path:'',
+  message:''
+]
+*/
