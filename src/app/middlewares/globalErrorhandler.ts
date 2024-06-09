dd4522b0f0a4e1efd82c6da9f2f44aee958ef425
searchsakib <searchsakib@gmail.com>
2024-06-09 15:09:27 +0600

    handleZodError.ts created

--- src/app/middlewares/globalErrorhandler.ts
+++ src/app/middlewares/globalErrorhandler.ts
@@ -1,9 +1,9 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { ErrorRequestHandler, NextFunction, Request, Response } from 'express';
-import { ZodError, ZodIssue } from 'zod';
+import { ErrorRequestHandler } from 'express';
+import { ZodError } from 'zod';
 import { TErrorSource } from '../interface/error';
-import { config } from 'dotenv';
+import handleZodError from '../errors/handleZodError';
 
 const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
   // setting default values
@@ -16,21 +16,6 @@ const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
       message: 'something went wrong',
     },
   ];
-
-  const handleZodError = (err: ZodError) => {
-    const errorSources: TErrorSource = err.issues.map((issue: ZodIssue) => {
-      return {
-        path: issue?.path[issue.path.length - 1],
-        message: issue.message,
-      };
-    });
-    statusCode = 400;
-    return {
-      statusCode,
-      message: 'Validation Error',
-      errorSources,
-    };
-  };
 
   if (err instanceof ZodError) {
     const simplifiedError = handleZodError(err);
